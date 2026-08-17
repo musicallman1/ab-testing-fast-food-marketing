a5d998d7f5e786c76d07e2bfe8060309f12c2a77
musicallman1 <hakeem@bedesigned.co>
2026-08-17 14:18:12 -0400

    Update SQL query to use new data source

--- queries/promo_total_sales_by_location.sql
+++ queries/promo_total_sales_by_location.sql
@@ -2,6 +2,6 @@ SELECT DISTINCT
    location_id AS location, 
    promotion, 
    ROUND(SUM(sales_in_thousands),2) AS sales 
-FROM `tc-da-1.turing_data_analytics.wa_marketing_campaign` 
+FROM `data_analytics.wa_marketing_campaign` 
 GROUP BY 
    1,2
