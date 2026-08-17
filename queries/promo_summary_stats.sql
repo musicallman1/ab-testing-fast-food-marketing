be1ee53d42fe946a7ae4d56cf8590bb7536f74fa
musicallman1 <hakeem@bedesigned.co>
2026-08-17 14:18:40 -0400

    Update SQL query to use new data source

--- queries/promo_summary_stats.sql
+++ queries/promo_summary_stats.sql
@@ -3,7 +3,7 @@ WITH total_promo_sales AS (
         location_id AS location,
         promotion,
         ROUND(SUM(sales_in_thousands), 2) AS sales
-    FROM `tc-da-1.turing_data_analytics.wa_marketing_campaign`
+    FROM `data_analytics.wa_marketing_campaign`
     GROUP BY
         1,
         2
